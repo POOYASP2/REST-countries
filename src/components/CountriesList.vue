fef1efcd13a4190ea3df0caaf4793e1a16e59f46
John Sharifi <pooyasharifi.sp2@gmail.com>
2023-10-03 13:46:02 +0330

    feat: add countries region filter

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
-import ACountry from 'components/ACountry.vue';
-import { reactive } from 'vue';
-import { api } from 'boot/axios';
 import type { CountryDetails } from 'src/types';
-const listOfContries = reactive<CountryDetails[]>([]);
-const loadCountries = async () => {
-  listOfContries.length = 0;
-  listOfContries.push(...(await api.get('/all')).data);
-};
-loadCountries();
+interface CountriesListProps {
+  listOfContries: Array<CountryDetails>;
+}
+const props = defineProps<CountriesListProps>();
+const listOfContries = props.listOfContries;
+
+import ACountry from 'components/ACountry.vue';
 </script>
 
 <template>
-  <div class="countries-wrapper">
-    <ACountry
-      v-for="country in listOfContries"
-      :key="country.name.official"
-      :flag="country.flags.png"
-      :name="country.name.official"
-      :population="country.population"
-      :region="country.region"
-      :capital="country.capital"
-    />
+  <div>
+    <div class="countries-wrapper__list" v-if="listOfContries.length > 0">
+      <ACountry
+        v-for="country in listOfContries"
+        :key="country.name.official"
+        :flag="country.flags.png"
+        :name="country.name.official"
+        :population="country.population"
+        :region="country.region"
+        :capital="country.capital"
+      />
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
-.countries-wrapper {
+.countries-wrapper__list {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   width: 100%;
