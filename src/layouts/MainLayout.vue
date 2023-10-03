9ee342dce2fc65d3477cf79b86527c29c0f338b7
John Sharifi <pooyasharifi.sp2@gmail.com>
2023-10-03 18:49:29 +0330

    feat: add dark theme
    - add dark theme toggle button
    - add dark theme for all pages
    - add dark theme in sotre

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,21 @@
+<script setup lang="ts">
+import { useTheme } from 'stores/theme';
+const theme = useTheme();
+</script>
 <template>
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white">
-      <q-toolbar class="header">
+      <q-toolbar :class="`header ${theme.darkMode ? 'header--dark' : null}`">
         <q-toolbar-title> Where in the world? </q-toolbar-title>
-        <button>Dark theme toggle</button>
+        <button @click="theme.ToggleTheme">Dark theme toggle</button>
       </q-toolbar>
     </q-header>
 
-    <q-page-container>
+    <q-page-container
+      :class="`page-countainer ${
+        theme.darkMode ? 'page-countainer--dark' : null
+      }`"
+    >
       <router-view />
     </q-page-container>
   </q-layout>
@@ -17,5 +25,19 @@
   padding: 20px;
   background: white;
   color: black;
+  button {
+    border: none;
+    outline: none;
+    padding: 12px;
+    border-radius: 14px;
+  }
+  &--dark {
+    background: $dark-blue;
+    color: white;
+    button {
+      background: $darker-blue;
+      color: white;
+    }
+  }
 }
 </style>
