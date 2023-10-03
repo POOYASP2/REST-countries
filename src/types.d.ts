87a4acf7e4ed1dd5b24c32680035e61babb3e094
John Sharifi <pooyasharifi.sp2@gmail.com>
2023-10-03 12:08:06 +0330

    chore: define `CountryDetails` interface

---
+++ src/types.d.ts
@@ -0,0 +1,83 @@
+export interface CountryDetails {
+  altSpellings: string[];
+  area: number;
+  capital?: string[];
+  capitalInfo?: {
+    latlng: number[];
+  };
+  car: {
+    signs: string[];
+    side: string;
+  };
+  cca2: string;
+  cca3: string;
+  ccn3: string;
+  coatOfArms: {
+    png: string;
+    svg: string;
+  };
+  continents: string[];
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  demonyms: {
+    eng: {
+      f: string;
+      m: string;
+    };
+    fra: {
+      f: string;
+      m: string;
+    };
+  };
+  flag: string;
+  flags: {
+    png: string;
+    svg: string;
+  };
+  idd: {
+    root: string;
+    suffixes: string[];
+  };
+  independent: boolean;
+  landlocked: boolean;
+  languages: {
+    [key: string]: string;
+  };
+  latlng: number[];
+  maps: {
+    googleMaps: string;
+    openStreetMaps: string;
+  };
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  population: number;
+  postalCode: {
+    format: string;
+    regex: string;
+  };
+  region: string;
+  startOfWeek: string;
+  status: string;
+  subregion: string;
+  timezones: string[];
+  tld: string[];
+  translations: {
+    [key: string]: {
+      official: string;
+      common: string;
+    };
+  };
+  unMember: boolean;
+}
