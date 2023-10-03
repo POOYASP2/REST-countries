b7d7137854c442e48d5ed560c94200c530230c19
John Sharifi <pooyasharifi.sp2@gmail.com>
2023-10-03 10:50:14 +0330

    chore: init porject

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar class="header">
+        <q-toolbar-title> Where in the world? </q-toolbar-title>
+        <button>Dark theme toggle</button>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<style lang="scss" scoped>
+.header {
+  padding: 20px;
+  background: white;
+  color: black;
+}
+</style>
